c4e1acb4815006711db523b3467ccfd9598e7591
Leith Kassous <lkassous17@gmail.com>
2025-04-18 00:04:50 +0100

    ✨ Add salle-service microservice

---
+++ salle-service/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/salles', methods=['GET'])
+def get_salles():
+    return jsonify([
+        {"id": 1, "nom": "Salle A"},
+        {"id": 2, "nom": "Salle B"}
+    ])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
